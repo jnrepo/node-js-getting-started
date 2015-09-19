2587e366f32e7e221ed40c1f065ea1707485cd1e
Joon Heo <joon.heo08@gmail.com>
2015-09-19 16:08:57 -0700

    added jade

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function(request, response) {
   var times = process.env.TIMES || 5;
   for (i=0; i < times; i++)
     result += cool();
-  response.send(result);
+  response.send('<div>' + result + '</div>');
 });
 
 app.get('/cool', function(request, response){
